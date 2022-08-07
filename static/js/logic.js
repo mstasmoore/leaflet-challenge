af3314abbe1fd10bb10e5907705f16b603346c5a
mstasmoore <101899938+mstasmoore@users.noreply.github.com>
2022-08-07 17:43:38 -0500

    save continued work

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,26 +7,59 @@ const createMap =((Data) => {
  });
 
 // street level view
-L.tileLayer(''https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-).addTo(myMap);
+    L.tileLayer(''https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(myMap);
 
 // map level
+    L.geoJSON(data, {
+        pointToLayer: function(feature, latlng){
+            return L.circleMarker(latlng, {
+                radius: getSize(feature.properties.mag),
+                fillColor: getColor(feature.properties.mag),
+                Color: "grey",
+                weight: 0.6,
+                opacity: 0.8,
+                fillopacity:1
+             }),
+        },
+        onEachFeature: onEachPopUp
+    }).addTo(myMap)
 
-l.geoJSON(data, {
-    pointToLayer: function(feature, latlng){
-        return L.circleMarker(latlng, {
-            radius: getSize(feature.properties.mag),
-            fillColor: getColor(feature.properties.mag),
-            Color: "grey",
-            weight: 0.6,
-            opacity: 0.8,
-            fillopacity:1
-        }),
-    },
-    onEachFeature: onEachPopUp
-}).addTo(myMap)
+    function onEachPopUp(feature,layer){
+        const format = d3.timeformat(%d-%b-%Y at %i:%M);
+        layer.bindPopup('`<strong>Place: </strong> ${feature.properties.place}<br><strong>Date and Time: </strong>${format(new Date(feature.properties.time))}<br><strong>Magnitude: </strong>${feature.properties.mag}`);
+    };
 
+//  legend layer
+    let legend = L.control({ position: "bottomleft"});
+    legend.onAdd = function() {
+        let div = L.DomUtil.create("div", "info legend");
+        let labels = [];
+        let magnitudes = [0,1,2,3,4,5];
+        let legendInfo = "h5>Magnitude</h5>";
+        div.innerHTML = legendInfo;
+        for (var i = 0; i <magnitudes.length; i++ {
+            labels.push ('<li style="background-color:' + getColor(magnitudes[i] + 1) + '"> <span>' + magnitudes[1] + (magnitudes[i+1]? '&ndash;' + magnitudes[i + 1] + '':'+') +'</span><li>');
+        }
+        div.innerHTML += "<ul" + labels.join("") + "</ul>";
+        return div;
+    };
+    legend.addTo(myMap);
+});
+
+// parameters for magnitude size
+const getSize = ((magnitude) => {
+    return magnitude * 5;
+});
+
+// magnitude colors
+const getColor = ((magnitude)=> {
+    switch(true) {
+        case(magnitude >5):
+            return ""
+    }
+})
 
 
 

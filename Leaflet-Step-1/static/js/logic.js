15661d78361f4853cd75f5e4513acacf122c37bf
mstasmoore <101899938+mstasmoore@users.noreply.github.com>
2022-08-12 17:58:28 -0500

    updated code

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -10,10 +10,7 @@ const createMap =((Data) => {
         zoom: mapZoom
  });
 
-//  base layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(myMap);
+
 
 // map level
     L.geoJSON(data, {
@@ -77,13 +74,27 @@ const getColor = ((magnitude)=> {
     }
 });
 
-d3.json(url).then(function(response) {
+d3.geoJSON(url).then(function(response) {
     createMap(response.features);
 
 
+// street level view
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                 '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                 'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.streets'
+}).addTo(mymap);
 
 });
+
+
+
+
+
 
 
 
